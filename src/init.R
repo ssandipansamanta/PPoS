dff1d532d0e0335fca9f9e82d6ad227a2a37fcb7
Sandipan Samanta <42437305+ssandipansamanta@users.noreply.github.com>
2021-01-07 16:28:33 +0100

    update init.R

--- src/init.R
+++ src/init.R
@@ -3,7 +3,7 @@
 # Example R code to install packages if not already installed
 #
 
-my_packages = c("shiny", "ggplot2", "markdown")
+my_packages = c("ggplot2", "markdown")
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
