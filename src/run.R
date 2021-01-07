ccf6330828c251b2ae8b18c934057771faa6ddf7
Sandipan Samanta <42437305+ssandipansamanta@users.noreply.github.com>
2021-01-07 16:16:45 +0100

    Adding init.R and update run.R file

--- src/run.R
+++ src/run.R
@@ -1,3 +1,9 @@
 library(shiny)
-folder_address = getwd()
-shiny::runApp(folder_address, launch.browser=F, port = 54321, host=getOption('shiny.host', "0.0.0.0"))
+
+port <- Sys.getenv('PORT')
+
+shiny::runApp(
+  appDir = getwd(),
+  host = '0.0.0.0',
+  port = as.numeric(port)
+)
